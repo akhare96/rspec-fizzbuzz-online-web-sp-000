67a1a6851751615ed7adc4866f94374cae8ce5c6
Ambar Khare <ambar.khare24@gmail.com>
2021-06-17 18:03:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(x)
+  if x % 3 == true
+    put "Fizz"
+  else if x % 5 == true
+    put "Buzz"
+  else if (x % 3 == true) && (x % 5 == true)
+    put "Fizzbuzz"
+  else nil
+  end
+end
